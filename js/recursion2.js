f6581875c1aca252ce01d065e4422caf15ae680a
jamiegledhill <32931760+jamiegledhill@users.noreply.github.com>
2023-01-23 20:51:47 +0000

    replaced count with len as exit condition

--- js/recursion2.js
+++ js/recursion2.js
@@ -1,75 +1,73 @@
-let maxLevel = 6;
-let minLevel = 0
-let currHeight = 0;
 let brown1;
 let brown2;
 let slider;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES)
-  noLoop()
-  brown1 = color('#655339');
-  brown2 = color('#4d4612');
-  green = color('#144e1b')
+  angleMode(DEGREES);
+  noLoop();
+  brown1 = color("#655339");
+  brown2 = color("#4d4612");
+  green = color("#144e1b");
   slider = createSlider(10, 90, 45);
   slider.position(10, windowHeight - 40);
-  slider.style('width', '200px'); 
+  slider.style("width", "200px");
   slider.input(draw);
 }
 
 function draw() {
   background(220);
-  resetMatrix()
-  count = maxLevel;
-  translate(windowWidth/2, windowHeight)
-  branch(250, count)
+  strokeJoin(ROUND);
+  resetMatrix();
+  translate(windowWidth / 2, windowHeight);
+  branch(250);
 }
-function branch(len, count){
-  let maxAngle = slider.value()
-  strokeWeight(map(len, 0, 300, 0, 10))
-  stroke(lerpColor(brown1, brown2, random(1)))
-  // stroke()
-  line(0,0,0, -len)
-  translate(0, -len)
-  if(--count >= minLevel){
-    if (count == minLevel){
+function branch(len) {
+  let maxAngle = slider.value();
+  strokeWeight(map(len, 20, 300, 0, 10));
+  stroke(lerpColor(brown1, brown2, random(1)));
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if (len > 20) {
+    if (len < 50) {
       let s = 15;
-      let r = 90 + random(-20, 20)
-      let g = 200 + random(-20, 20)
-      let b = 50+ random(-20, 20)
-      fill(r, g, b, 100)
-      noStroke()
+      // let r = 90 + random(-20, 20)
+      // let g = 200 + random(-20, 20)
+      // let b = 50+ random(-20, 20)
+      let r = 200 + random(-20, 20);
+      let g = 20 + random(-20, 20);
+      let b = 40 + random(-20, 20);
+      fill(r, g, b, 100);
+      noStroke();
       // triangle(-s/2, 0, s/2, 0, -0, -s)
-      beginShape()
-      let rad = random(10, 30)
-      for(let i = 45; i < 135; i++){
+      beginShape();
+      let rad = random(10, 30);
+      for (let i = 45; i < 135; i++) {
         let x = rad * cos(i);
-        let y = rad * sin(i)
-        vertex(x, y)
+        let y = rad * sin(i);
+        vertex(x, y);
       }
-      for(let i = 135; i > 45; i--){
+      for (let i = 135; i > 45; i--) {
         let x = rad * cos(i);
         let y = rad * sin(-i) + 10;
-        vertex(x, y)
+        vertex(x, y);
       }
-      endShape(CLOSE)
-    }else{
-      
-      push()
-      rotate(random(-maxAngle, maxAngle))
-      branch(len*.77, count)
-      pop() 
-      push()
-      rotate(random(-maxAngle, maxAngle))
-      branch(len*.77, count)
+      endShape(CLOSE);
+    } else {
+      push();
+      rotate(random(-maxAngle, maxAngle));
+      branch(len * 0.67);
       pop();
-      push()
-      rotate(random(-maxAngle, maxAngle))
-      branch(len*.77, count)
+      push();
+      rotate(random(-maxAngle, maxAngle));
+      branch(len * 0.67);
       pop();
-      push()
-      rotate(random(-maxAngle, maxAngle))
-      branch(len*.77, count)
+      push();
+      rotate(random(-maxAngle, maxAngle));
+      branch(len * 0.67);
+      pop();
+      push();
+      rotate(random(-maxAngle, maxAngle));
+      branch(len * 0.5);
       pop();
     }
   }
